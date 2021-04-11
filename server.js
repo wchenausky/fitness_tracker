f47872ef9174e19a051f40910e3c62499132f9b4
William Cheausky <williamchenausky@yahoo.com>
2021-04-11 11:26:21 -0400

    set up server

---
+++ server.js
@@ -0,0 +1,32 @@
+// Require dependencies
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Port variable
+const PORT = 3000;
+
+const app = express();
+
+// Use epress to parse url encdoed bodies
+
+app.use(express.urlencoded({ extended: true }));
+// express to aprse middleware data to JSON
+app.use(express.json());
+
+// Set up static files form the public folder (not on the internet)
+app.use(express.static("public"));
+
+// Setup mongoose connection
+mongoose.connect("mongodb://localhost/workouts",{
+    useNewUrlParse: true,
+    useFindAndModify: false,
+});
+
+// routes
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+// listener got connections
+app.listen(PORT, () => {
+    console.log('listening on port ${PORT');
+});
